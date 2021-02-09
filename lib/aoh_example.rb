8af32c09e7d79398ffec3b9685c53a963bd67243
Jose Mendoza <pball626@gmail.com>
2021-02-09 17:32:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,7 +6,7 @@ JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = {name:  "Bruce Green", occupation: "Fan of Mildred"}
-
+binding.pry
 def assembled_aoh
   assembled_aoh = [
     { name:  "Don Gately", occupation: "Live-in Staff"},
