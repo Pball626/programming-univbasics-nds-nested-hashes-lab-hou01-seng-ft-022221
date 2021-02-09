6bed4a97f7250d41fa35306e3df78736afd40afe
Jose Mendoza <pball626@gmail.com>
2021-02-09 17:14:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   assembled_aoh = {
-    Don_G 
-    JOELLE_VD 
-    PAT_M 
-    KATE_G 
-    BRUCE_E
+    
   }
 end
 
