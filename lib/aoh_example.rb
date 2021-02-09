4dc367833e56324a98108391728b5dab48c6f4fb
Jose Mendoza <pball626@gmail.com>
2021-02-09 17:27:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,13 +9,12 @@ BRUCE_G = {name:  "Bruce Green", occupation: "Fan of Mildred"}
 
 def assembled_aoh
   assembled_aoh = [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-    ]
-    assembled_aoh
+    {name:  "Don Gately", occupation: "Live-in Staff"},
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    {name:  "Bruce Green", occupation: "Fan of Mildred"}
+  ]
 end
 
 def literal_aoh[
